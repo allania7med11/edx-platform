78e2e188f85cb08de2034a61c3ea6485c361da9b
allania7med11 <allania7med11@gmail.com>
2023-08-12 20:09:06 +0000

    add greeting model

--- openedx/core/djangoapps/greeting/models.py
+++ openedx/core/djangoapps/greeting/models.py
@@ -1,1 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import User  
 
+class Greeting(models.Model):
+    text = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, db_index=True, on_delete=models.CASCADE)
