43e1dfa99eabb079f8239b3bd72101aa2d2b7995
allania7med11 <allania7med11@gmail.com>
2023-08-12 21:04:08 +0000

    remove user from Greeting

--- openedx/core/djangoapps/greeting/models.py
+++ openedx/core/djangoapps/greeting/models.py
@@ -4,4 +4,3 @@ from django.contrib.auth.models import User
 class Greeting(models.Model):
     text = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, db_index=True, on_delete=models.CASCADE)
