25cdf3dcca15254619fbf0ccc79f991155005eff
allania7med11 <allania7med11@gmail.com>
2023-08-21 22:40:13 +0000

    log greeting creation

--- openedx/core/djangoapps/greetings/views.py
+++ openedx/core/djangoapps/greetings/views.py
@@ -1,3 +1,4 @@
+import logging
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -5,10 +6,13 @@ from edx_rest_framework_extensions.auth.jwt.authentication import JwtAuthenticat
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
+from openedx.core.djangoapps.greetings.models import Greeting
 from openedx.core.djangoapps.greetings.serializers import GreetingSerializer
 
 from openedx.core.lib.api.authentication import BearerAuthentication
 
+
+log = logging.getLogger(__name__)
 
 class GreetingsCreateView(APIView):
     authentication_classes = (JwtAuthentication, BearerAuthentication, SessionAuthentication,)
@@ -17,7 +21,8 @@ class GreetingsCreateView(APIView):
     def post(self, request):
         serializer = GreetingSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            greeting: Greeting  = serializer.save()
+            log.info(f"Greeting {greeting.text} at {greeting.created_at}")
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
